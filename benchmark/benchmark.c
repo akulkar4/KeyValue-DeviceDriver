5b5d6f5e96113659f42dcb0ee64630445c11c415
root <activebunny@gmail.com>
2016-09-28 13:44:27 -0700

    Updated the benchmark

--- benchmark/benchmark.c
+++ benchmark/benchmark.c
@@ -11,14 +11,15 @@ int main(int argc, char *argv[])
     int a;
     int tid;
     __u64 size;
+    char data[1024];
     int devfd;
     if(argc < 3)
     {
-        fprintf(stderr, "Usage: %s number_of_threads number_of_keys number_of_transactions\n",argv[0]);
+        fprintf(stderr, "Usage: %s number_of_keys number_of_transactions\n",argv[0]);
         exit(1);
     }
-    number_of_keys = atoi(argv[2]);
-    number_of_transactions = atoi(argv[3]);
+    number_of_keys = atoi(argv[1]);
+    number_of_transactions = atoi(argv[2]);
     devfd = open("/dev/keyvalue",O_RDWR);
     if(devfd < 0)
     {
@@ -29,16 +30,19 @@ int main(int argc, char *argv[])
     // Initializing the keys
     for(i = 0; i < number_of_keys; i++)
     {
+        memset(data, 0, 1024);
         a = rand();
-        tid = kv_set(devfd,i,sizeof(int),&a);
-        fprintf(stderr,"S\t%d\t%d\t%d\n",tid,sizeof(int),a);
+        sprintf(data,"%d",a);
+        tid = kv_set(devfd,i,strlen(data),data);
+        fprintf(stderr,"S\t%d\t%d\t%d\t%s\n",tid,i,strlen(data),data);
     }
-    for(i = 0; i < number_of_transactions; i++)
+/*    for(i = 0; i < number_of_transactions; i++)
     {
         tid = kv_get(devfd,i,size,&a);
         fprintf(stderr,"G\t%d\t%d\t%d\n",tid,sizeof(int),a);
         
-    }
+    }*/
     close(devfd);
     return 0;
 }
+
